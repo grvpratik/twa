fd52e2ad16dc261ca201f0528ab2f42a3ea44cbc
Pratik Grv <pratikgauravv@gmail.com>
2024-09-08 00:09:19 +0530

    telegram ui

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useTelegram } from '@/provider/telegram-provider'
+import { Button } from '@telegram-apps/telegram-ui'
 import React from 'react'
 
 const HomePage = () => {
@@ -16,10 +17,20 @@ const HomePage = () => {
           <pre>{JSON.stringify(user, null, 2)}</pre>
           Eniter Web App data:
                   <pre>{JSON.stringify(webApp, null, 2)}</pre>
-
+<Button
+  mode="filled"
+  size="s"
+>
+  Action
+</Button>
         </div>
       ) : (
-        <div>Make sure web app is opened from telegram client</div>
+        <div>Make sure web app is opened from telegram client <Button
+  mode="filled"
+  size="s"
+>
+  Action
+</Button></div>
       )}
     </div>
   )
