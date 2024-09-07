6e339bcf2b9073886464cbbc8762f6977e6afabb
Pratik Grv <pratikgauravv@gmail.com>
2024-09-07 14:44:56 +0530

    telegram web app init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
+import Script from "next/script";
+import { TelegramProvider } from "@/lib/telegram-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+         <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive"></Script>
+      </head>
+      <body className={inter.className}><TelegramProvider>{children}</TelegramProvider></body>
     </html>
   );
 }
