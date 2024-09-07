63fce8338153c9f107c39e6625496151181f97f1
Pratik Grv <pratikgauravv@gmail.com>
2024-09-08 01:22:03 +0530

    themes check 3

--- provider/theme-provider.tsx
+++ provider/theme-provider.tsx
@@ -2,7 +2,10 @@
 'use client'
 
 import { ThemeProvider } from 'next-themes'
+import { useTelegramTheme } from './telegram-theme'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider attribute="class" defaultTheme="system" enableSystem>{children}</ThemeProvider>
+ const theme= useTelegramTheme()
+ 
+  return <ThemeProvider attribute="class" defaultTheme={theme} >{children}</ThemeProvider>
 }
