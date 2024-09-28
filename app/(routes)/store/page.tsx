435e133482d5605e7f78680e6baf797a89631543
grvPratik <pratikgauravv@gmail.com>
2024-09-28 20:01:54 +0530

    wallet connect test 2

--- app/(routes)/store/page.tsx
+++ app/(routes)/store/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { useState } from "react";
 import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { Button } from "@/components/ui/button";
 
 const StorePage = () => {
 	const [walletAddress, setWalletAddress] = useState(null);
@@ -17,16 +18,41 @@ const StorePage = () => {
 			alert("Phantom wallet not found! Please install it.");
 		}
 	};
+	const BOT_USERNAME='open_guild_bot'
+function redirectToTelegramChat(submissionId: string) {
+	const encodedSubmissionId = encodeURIComponent(submissionId);
+	// Encode the submission ID to ensure it's URL-safe
+	// Construct the deep link URL to open the chat directly
+	const webFallback = `https://t.me/${BOT_USERNAME}?start=${encodedSubmissionId}`;
+	const deepLink = `phantom://22.04.11/ul/v1/connect?app_url=${encodeURIComponent(
+		"https://aphanfts.app/"
+	)}&dapp_encryption_public_key=CfK99Zo6zcpiZMDTveXYTEJXouvrNd2cKw9C1yVQSNAR&redirect_link=${webFallback}&cluster=devnet`;
+	// Fallback URL for web clients
+	
+	// Try to open the Telegram app
+	window.location.href = deepLink;
+	// If the Telegram app doesn't open within 100ms, redirect to the web version
+	setTimeout(() => {
+		if (document.hidden) {
+			return; // The app was opened
+		}
+		window.location.href = webFallback;
+	}, 100);
+}
 
 	return (
-		<div>
-			<h1>Solana Phantom Wallet Connection</h1>
-			{walletAddress ? (
-				<p>Connected: {walletAddress}</p>
-			) : (
-				<button onClick={connectWallet}>Connect Phantom Wallet</button>
-			)}
-		</div>
+		<>
+			<div>
+				<h1>Solana Phantom Wallet Connection</h1>
+				{walletAddress ? (
+					<p>Connected: {walletAddress}</p>
+				) : (
+					<button onClick={connectWallet}>Connect Phantom Wallet</button>
+				)}
+			</div>
+
+			<Button onClick={() => redirectToTelegramChat("hii")}>open</Button>
+		</>
 	);
 };
 
